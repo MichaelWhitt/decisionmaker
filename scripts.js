af285ea95ee8df267b6c81b226f38dc5b080d411
Michael Whitt <michaelwhitt90@gmail.com>
2021-07-28 15:39:10 -0500

    18th commit huge update

--- scripts.js
+++ scripts.js
@@ -1,37 +1,37 @@
-function addSpin(){
-    // 20-21 rotations for variation.
-    var x = 7177; //min value
-    var y = 7536; //max value
-    //Wheel 8 degree values
-    /*span 1 degrees = 7357 - 7400
-    span 2 degrees = 7177 - 7220 
-    span 3 degrees = 7447 - 7491
-    span 4 degrees = 7267- 7310
-    span 5 degrees = 7221 - 7266 
-    span 6 degrees = 7401 - 7446
-    span 7 degrees = 7311 - 7356
-    span 8 degrees = 7492 - 7536*/
-    //works even on multiple clicks, but doesn't spin much since it's already gone through 20 spins. 
-    //works only for 8 wheel. Must change degrees for all other variations.
+// function addSpin(){
+//     // 20-21 rotations for variation.
+//     var x = 7177; //min value
+//     var y = 7536; //max value
+//     //Wheel 8 degree values
+//     /*span 1 degrees = 7357 - 7400
+//     span 2 degrees = 7177 - 7220 
+//     span 3 degrees = 7447 - 7491
+//     span 4 degrees = 7267- 7310
+//     span 5 degrees = 7221 - 7266 
+//     span 6 degrees = 7401 - 7446
+//     span 7 degrees = 7311 - 7356
+//     span 8 degrees = 7492 - 7536*/
+//     //works even on multiple clicks, but doesn't spin much since it's already gone through 20 spins. 
+//     //works only for 8 wheel. Must change degrees for all other variations.
 
     
-    var deg = Math.floor(Math.random() * (x - y)) + y;
+//     var deg = Math.floor(Math.random() * (x - y)) + y;
 
-    let z = document.getElementById('box').style.transform = "rotate("+deg+"deg)";
-    console.log(z);
+//     let z = document.getElementById('wheel').style.transform = "rotate("+deg+"deg)";
+//     console.log(z);
 
-    var element = document.getElementById("mainbox");
-    element.classList.remove("animate");
-    setTimeout(function(){
-        element.classList.add("animate");
-    }, 5000); //5000 = 5s;
-}
+//     var element = document.getElementById("d1");
+//     element.classList.remove("animate");
+//     setTimeout(function(){
+//         element.classList.add("animate");
+//     }, 5000); //5000 = 5s;
+// }
 
 /*Background video playback speed
 document.querySelector('video').playbackRate = .75;*/
 
 /*Changes span to some new text - make JS for input here*/
-//Old Code for wheel inputs
+
 
 
 
@@ -56,6 +56,51 @@ function changeInput(){
 
 
 function generateWheel(array) {
+    let wheelContainer = document.getElementById("wheel");
+    // reset container
+    wheelContainer.innerHTML = "";
+    let d1 = document.createElement("div");
+    let spinButton = document.createElement("button");
+    d1.id = "d1";
+    d1.className = "d1";
+    spinButton.id = "spin";
+    spinButton.textContent = "Spin!";
+ 
+
+    wheelContainer.appendChild(d1);
+    wheelContainer.appendChild(spinButton);
+
+    spinButton.onclick = function addSpin() {
+      var x = 2;
+      var y = 500;
+      var deg = Math.floor(Math.random() * (x - y)) + y;
+
+        let z = document.getElementById('d1').style.transform = "rotate("+deg+"deg)";
+        // console.log(z);
+
+        var element = document.getElementById("d1");
+        element.classList.remove("animate");
+        setTimeout(function(){
+            element.classList.add("animate");
+        }, 5000); //5000 = 5s;
+        
+    }
+    // add reset wheel if spin is clicked again
+    
+    for (let i = 0; i <= array.length -1; i++) {
+      let span = document.createElement("span");
+      let boldText = document.createElement("b");
+      boldText.id = "spanb" + (i + 1);
+      boldText.textContent = array[i];
+      span.classList.add("span" + (i + 1) + "w" + array.length);
+      span.appendChild(boldText);
+      d1.appendChild(span);
+
+    }
+  }
+
+  // old code
+  /*function generateWheel(array) {
     let s = document.getElementById("wheel");
     // reset container
     s.innerHTML = "";
@@ -85,78 +130,5 @@ function generateWheel(array) {
       span.appendChild(boldText);
       d4.appendChild(span);
 
-}
-
-/*New Code to generate wheel on update wheel click
-function changeInput(){
-    
-    let totalNumInputs = 0;
-    for (let i= 1; i<9; i++){
-        
-        let inputVal = document.getElementById("input"+i).value;
-        document.getElementById("spanb"+i).innerHTML = inputVal;
-        totalNumInputs += 1;
-        console.log(totalNumInputs);
-        generateWheel(totalNumInputs);
     }
-
-    //Generate Wheel Function
-    function generateWheel(num) {
-        let s = document.getElementById("wheel");
-        // reset container
-        s.innerHTML = "";
-        let d1 = document.createElement("div");
-        let d2 = document.createElement("div");
-        let d3 = document.createElement("div");
-        let d4 = document.createElement("div");
-        d1.id = "d1";
-        d1.className = "d1";
-        d2.id = "d2";
-        d2.className = "d2";
-        d3.id = "d3";
-        d3.className = "d3";
-        d4.id = "d4";
-        d4.className = "d4";
-    
-          s.appendChild(d1);
-          d1.appendChild(d2);
-          d2.appendChild(d3);
-          d3.appendChild(d4);
-        for (let i = 1; i <= num; i++) {
-          let span = document.createElement("span");
-          let spanb = document.createElement("b");
-          span.classList.add("span"+i+"w"+num);
-          d4.appendChild(span);
-          //b.classList.add("spanb"+i);
-          //span.appendChild();
-        }
-    } 
-
-    //Code to generate wheels depending on chosen inputs
-    let wheelNum = totalNumInputs;
-    switch(wheelNum) {
-        case 2:
-            generateWheel(2);
-          break;
-        case 3:
-            generateWheel(3);
-          break;
-        case 4:
-            generateWheel(4);
-          break;
-        case 5:
-            generateWheel(5);
-          break;
-        case 6:
-            generateWheel(6);
-          break;
-        case 7:
-            generateWheel(7);
-          break;
-        case 8:
-            generateWheel(8);
-        //default:
-      }
-}
-*/
-}
+  }*/
