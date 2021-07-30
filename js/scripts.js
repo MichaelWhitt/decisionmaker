ac1adf8b0308d317a190b5f9ef7e60ea3281abbc
Michael Whitt <michaelwhitt90@gmail.com>
2021-07-30 16:27:10 -0500

    34th commit added js notes at bottom

--- js/scripts.js
+++ js/scripts.js
@@ -94,3 +94,52 @@ $(function(){
         $("#accountModal").modal("toggle")
     });
 });
+
+
+//NOTES
+//Wheel 2 degree values
+    // span 1 degrees (light blue) = 7291 - 7470
+    // span 2 degrees (light green) = 7110 - 7290
+//Wheel 3 degree values
+    // span 1 degrees (light blue) = 7291 - 7411
+    // span 2 degrees (light green) = 7170 - 7290
+    // span 3 degrees (dark pink) =  7412 - 7528
+//Wheel 4 degree values
+    // span 1 degrees (light blue) = 7291 - 7380
+    // span 2 degrees (light green) = 7201 - 7290
+    // span 3 degrees (dark pink) =  7381 - 7469
+    // span 4 degrees (orange) = 7470 - 7559
+//Wheel 5 degree values
+    // span 1 degrees (light blue) = 7291 - 7363
+    // span 2 degrees (light green) = 7217 - 7290
+    // span 3 degrees (dark pink) =  7364 - 7430
+    // span 4 degrees (orange) = 7150 - 7216
+    // span 5 degrees = (yellow) = 7431 - 7509
+//Wheel 6 degree values
+    // span 1 degrees (light blue) = 7291 - 7349
+    // span 2 degrees (light green) = 7232 - 7290
+    // span 3 degrees (dark pink) =  7350 - 7410
+    // span 4 degrees (orange) = 7170 - 7231
+    // span 5 degrees = (yellow) = 7411 - 7470
+    // span 6 degrees (dark blue) = 7471 -7529
+//Wheel 7 degree values
+    // span 1 degrees (light blue) = 7291 - 7344 (53)
+    // span 2 degrees (light green) = 7236 - 7290 (54)
+    // span 3 degrees (dark pink) = 7345 - 7391 (46) 
+    // span 4 degrees (orange) = 7189 - 7235 (46)
+    // span 5 degrees = (yellow) = 7392 - 7439 (47)
+    // span 6 degrees (dark blue) = 7500 - 7548 (48)
+    // span 7 degrees (purple) = 7440 - 7499 (59)
+//Wheel 8 degree values
+    /*
+    span 1 degrees (light blue)= 7291 - 7335
+    span 2 degrees (light green) = 7246 - 7290 
+    span 3 degrees (dark salmon)= 7336 - 7380
+    span 4 degrees (salmon) = 7200 - 7245 
+    span 5 degrees (yellow) = 7426 - 7470
+    span 6 degrees (dk blue) = 7155 - 7199(6) (45)
+    span 7 degrees (purple) =  7471 - 7515
+    span 8 degrees (pink) = 7381 - 7425
+    */
+
+    //works even on multiple clicks, but doesn't spin much since it's already gone through 20 spins. 
